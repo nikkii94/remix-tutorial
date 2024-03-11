4b118df5f5f88bcc5404440f98950593eb991793
nikkii94 <k.nikolett17@gmail.com>
2024-03-11 13:50:49 +0100

    Add stylesheet

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,15 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import appStylesHref from './app.css?url';
+import { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  {
+    rel: 'stylesheet', href: appStylesHref
+  }
+];
+
 export default function App() {
   return (
     <html lang="en">
