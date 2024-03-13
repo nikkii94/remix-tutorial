945a5a4af443f7d2921d0032419a5bbde8e6ab54
nikkii94 <k.nikolett17@gmail.com>
2024-03-13 12:54:28 +0100

    Remix tutorial finished

--- app/root.tsx
+++ app/root.tsx
@@ -1,86 +1,137 @@
 import {
-  Form,
-  Link,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
+    Form,
+    Links,
+    Meta,
+    NavLink,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+    useLoaderData,
+    useNavigation,
+    useSubmit,
 } from "@remix-run/react";
 
 import appStylesHref from './app.css?url';
-import { LinksFunction, json } from "@remix-run/node";
-import { getContacts } from "./data";
+import {LinksFunction, json, redirect, LoaderFunctionArgs} from "@remix-run/node";
+import {createEmptyContact, getContacts} from "./data";
+import {useEffect} from "react";
 
 export const links: LinksFunction = () => [
-  {
-    rel: 'stylesheet', href: appStylesHref
-  }
+    {
+        rel: 'stylesheet', href: appStylesHref
+    }
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({request}: LoaderFunctionArgs) => {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
 
-  return json({ contacts });
+    return json({contacts, q});
+};
+
+export const action = async () => {
+    const contact = await createEmptyContact();
+
+    return redirect(`/contacts/${contact.id}/edit`);
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
-  console.log(contacts)
+    const {contacts, q} = useLoaderData<typeof loader>();
+    const navigation = useNavigation();
+    const submit = useSubmit();
 
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
+    const searching =
+        navigation.location &&
+        new URLSearchParams(navigation.location.search).has(
+            "q"
+        );
+
+    useEffect(() => {
+        const searchField = document.getElementById("q");
+        if (searchField instanceof HTMLInputElement) {
+            searchField.value = q || "";
+        }
+    }, [q]);
+
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
         <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (<>{contact.first} {contact.last}</>) : (<i>No name</i>) }{" "}
-                      {contact.favorite ? (
-                        <span>★</span>
-                      ) : null}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              
-            ) : (
-              <p><i>No contacts</i></p>
-            )}
-          </nav>
+            <h1>Remix Contacts</h1>
+            <div>
+                <Form
+                    id="search-form"
+                    role="search"
+                    onChange={(event) => {
+                        const isFirstSearch = q === null;
+                        submit(event.currentTarget, {
+                            replace: !isFirstSearch,
+                        });
+                    }}
+                >
+                    <input
+                        id="q"
+                        aria-label="Search contacts"
+                        className={searching ? "loading" : ""}
+                        placeholder="Search"
+                        type="search"
+                        name="q"
+                        defaultValue={q || ""}
+                    />
+                    <div id="search-spinner" aria-hidden hidden={!searching} />
+                </Form>
+                <Form method="post">
+                    <button type="submit">New</button>
+                </Form>
+            </div>
+            <nav>
+                {contacts.length ? (
+                    <ul>
+                        {contacts.map((contact) => (
+                            <li key={contact.id}>
+                                <NavLink
+                                    className={({isActive, isPending}) =>
+                                        isActive
+                                            ? "active"
+                                            : isPending
+                                                ? "pending"
+                                                : ""
+                                    }
+                                    to={`contacts/${contact.id}`}>
+                                    {contact.first || contact.last ? (<>{contact.first} {contact.last}</>) : (
+                                        <i>No name</i>)}{" "}
+                                    {contact.favorite ? (
+                                        <span>★</span>
+                                    ) : null}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+
+                ) : (
+                    <p><i>No contacts</i></p>
+                )}
+            </nav>
         </div>
-        <div id="detail">
-          <Outlet />
+        <div
+            className={
+                navigation.state === "loading" && !searching
+                    ? "loading"
+                    : ""
+            }
+            id="detail">
+            <Outlet/>
         </div>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,12 +1,12 @@
-import { Form, json, useLoaderData } from "@remix-run/react";
+import {Form, json, useFetcher, useLoaderData} from "@remix-run/react";
 import type { FunctionComponent } from "react";
 
-import { getContact, type ContactRecord } from "../data";
-import { LoaderFunctionArgs } from "@remix-run/node";
+import {getContact, type ContactRecord, updateContact} from "~/data";
+import type {ActionFunctionArgs, LoaderFunctionArgs} from "@remix-run/node";
 import invariant from "tiny-invariant";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  invariant(params.contactId, "Missing contactId param");
+  invariant(params.contactId, 'Missing contactId param');
   const contact = await getContact(params.contactId);
 
   if (!contact) {
@@ -15,6 +15,15 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
   return json({ contact });
 };
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+    invariant(params.contactId, 'Missing contactId param');
+
+    const formData = await request.formData();
+    return updateContact(params.contactId, {
+        favorite: formData.get("favorite") === "true",
+    });
+}
 
 export default function Contact() {
   const { contact } = useLoaderData<typeof loader>();
@@ -83,10 +92,13 @@ export default function Contact() {
 const Favorite: FunctionComponent<{
   contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
-  const favorite = contact.favorite;
+  const fetcher = useFetcher();
+    const favorite = fetcher.formData
+        ? fetcher.formData.get("favorite") === "true"
+        : contact.favorite;
 
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
         name="favorite"
@@ -94,6 +106,6 @@ const Favorite: FunctionComponent<{
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 };
